be7e03f0d3a20349f8f1129a3d32766aaa72cd30
sg2hex <sg2hex@gmail.com>
2025-10-09 04:53:01 +0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,29 @@
+class Vehicle {
+    public void power_Boost() {
+        System.out.println("Vehicle is boosting power...");
+    }
+}
+
+// Child class
+public class Car extends Vehicle {
+    int power = 2000;
+    String model = "Nissan Almera 1.2";
+
+    public static void powerUp() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Power level: " + i);
+        }
+    }
+
+    @Override
+    public void power_Boost() {
+        System.out.println("Power: " + power);
+        System.out.println("Model: " + model);
+    }
+
+    public static void main(String[] args) {
+        Car.powerUp();
+        Car pow1 = new Car();
+        pow1.power_Boost();
+    }
+}
